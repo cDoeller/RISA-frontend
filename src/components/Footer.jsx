f89caf95280429d74edac2693f1d1bd0ab66ceaa
Christian Doeller <mail@christiandoeller.de>
2024-06-04 23:14:09 +0200

    footer blueprint

---
+++ src/components/Footer.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import "../styles/Footer.css";
+import { Link } from "react-router-dom";
+
+function Footer() {
+  return (
+    <div className="footer-wrapper flex-column-center">
+      <div className="footer-project-info flex-column-center">
+        <p className="footer-project-info-p">
+          An Artistic Research Project by Christian Doeller
+        </p>
+        <p className="footer-project-info-p">
+          Copyright 2024 @ the <Link to="#">contributors</Link>
+        </p>
+      </div>
+
+      <div className="footer-socialmedia flex-column-center">
+        <p className="footer-follow-p">follow us on</p>
+        <button className="footer-follow-button button">instagram</button>
+      </div>
+
+      <ul className="footer-legal-ul flex-row-center pointer">
+        <Link to="#">
+          <li className="footer-legal-li">imprint</li>
+        </Link>
+        <Link to="#">
+          <li className="footer-legal-li">data</li>
+        </Link>
+        <Link to="#">
+          <li className="footer-legal-li">contact</li>
+        </Link>
+        <Link to="#">
+          <li className="footer-legal-li">FAQ</li>
+        </Link>
+      </ul>
+    </div>
+  );
+}
+
+export default Footer;
