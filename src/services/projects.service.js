dc3fa3c0fc38b78632365f61d127297c53ffe95c
Christian Doeller <mail@christiandoeller.de>
2024-06-10 17:19:15 +0200

    added service structure

---
+++ src/services/projects.service.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+class ProjectsService {
+  // ** BASE URL and Token (to do)
+  constructor() {
+    this.api = axios.create({
+      baseURL: import.meta.env.VITE_API_URL || "http://localhost:5005",
+    });
+  }
+
+  // ** HTTP Methods
+
+  // GET
+  getAllProjects = () => {
+    return this.api.get("/api/projects");
+  };
+}
