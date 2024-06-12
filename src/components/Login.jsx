75d6ca9da70ea59592e6a1f7f59dcfb0df66fd28
Christian Doeller <mail@christiandoeller.de>
2024-06-12 16:25:45 +0200

    added login function

---
+++ src/components/Login.jsx
@@ -0,0 +1,93 @@
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useState, useContext } from "react";
+import { AuthContext } from "../context/auth.context";
+import authService from "../services/auth.service";
+import "../styles/styles-components/Login.css";
+
+function Login() {
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const navigate = useNavigate();
+  const { authenticateUser } = useContext(AuthContext);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const body = {
+      email: email,
+      password: password,
+    };
+
+    authService
+      .login(body)
+      .then((response) => {
+        // save token (response of login route) in local storage
+        const token = response.data.authToken;
+        console.log(token);
+        localStorage.setItem("authToken", token);
+
+        // check if token correct: verify route & local storage
+        authenticateUser();
+
+        // navigate to profile page
+        navigate("/");
+      })
+      .catch((err) => {
+        setErrorMessage(err.response.data.message);
+        console.log(err);
+      });
+  };
+
+  const errorMessageElement = (
+    <>
+      <h2 className="error-message">{errorMessage}</h2>
+    </>
+  );
+
+  return (
+    <div className="login-wrapper page-wrapper">
+      <form
+        action=""
+        onSubmit={(e) => {
+          handleSubmit(e);
+        }}
+        className="form"
+      >
+        <label htmlFor="" className="form-input-label">
+          email
+          <input
+            type="email"
+            name="email"
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            value={email}
+            required
+            className="form-input-input"
+          />
+        </label>
+        <label htmlFor="" className="form-input-label">
+          password
+          <input
+            type="password"
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            value={password}
+            required
+            className="form-input-input"
+          />
+        </label>
+        {errorMessage && errorMessageElement}
+        <button type="submit" className="form-button">
+          Log In
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -2,7 +2,7 @@ import { React, useState, useEffect, useContext } from "react";
 import "../styles/styles-pages/AdminPage.css";
 import projectsService from "../services/projects.service";
 import { AuthContext } from "../context/auth.context";
-import authService from "../services/auth.services";
+import Login from "../components/Login";
 
 function AdminPage() {
   const [projects, setProjects] = useState(null);
@@ -20,37 +20,46 @@ function AdminPage() {
   }, []);
 
   return (
-    <div className="page-wrapper">
-      <section className="admin-project-section flex-column-left">
-        <div className="admin-search-wrapper flex-row-left">
-          <label className="admin-label flex-row">
-            Projects
-            <input className="admin-input" type="text" />
-          </label>
-          <div className="admin-add-button image-wrapper pointer">
-            <img
-              src="https://cdn-icons-png.flaticon.com/512/54/54570.png"
-              alt=""
-            />
-          </div>
+    <>
+      {!isLoggedIn ? (
+        <Login />
+      ) : (
+        <div className="page-wrapper">
+          <section className="admin-project-section flex-column-left">
+            <div className="admin-search-wrapper flex-row-left">
+              <label className="admin-label flex-row">
+                Projects
+                <input className="admin-input" type="text" />
+              </label>
+              <div className="admin-add-button image-wrapper pointer">
+                <img
+                  src="https://cdn-icons-png.flaticon.com/512/54/54570.png"
+                  alt=""
+                />
+              </div>
+            </div>
+            <div className="admin-list-container flex-column-left">
+              {projects &&
+                projects.map((project) => {
+                  return (
+                    <div
+                      key={project._id}
+                      className="admin-card-wrapper pointer"
+                    >
+                      <h1 className="admin-card-title">
+                        {project.title}, {project.year}
+                      </h1>
+                      {project.contributors.length > 0 && (
+                        <p className="admin-card-subtitle">bla</p>
+                      )}
+                    </div>
+                  );
+                })}
+            </div>
+          </section>
         </div>
-        <div className="admin-list-container flex-column-left">
-          {projects &&
-            projects.map((project) => {
-              return (
-                <div key={project._id} className="admin-card-wrapper pointer">
-                  <h1 className="admin-card-title">
-                    {project.title}, {project.year}
-                  </h1>
-                  {project.contributors.length > 0 && (
-                    <p className="admin-card-subtitle">bla</p>
-                  )}
-                </div>
-              );
-            })}
-        </div>
-      </section>
-    </div>
+      )}
+    </>
   );
 }
 
