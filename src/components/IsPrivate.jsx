91d058b7a316f52b889f5c4403f659efb1629b84
Christian Doeller <mail@christiandoeller.de>
2024-06-12 15:56:31 +0200

    added isPrivate component

---
+++ src/components/IsPrivate.jsx
@@ -0,0 +1,21 @@
+import React, { useContext } from "react";
+import { AuthContext } from "../context/auth.context";
+import { Navigate } from "react-router-dom";
+
+// everything wrapped in this component:
+// > only show children if * logged in *
+// > redirect if not logged in
+
+function IsPrivate(props) {
+  const { isLoggedIn } = useContext(AuthContext);
+
+  // if logged in, show the content
+  if (isLoggedIn) {
+    return props.children;
+    // if not logged in, navigate to login page
+  } else {
+    return <Navigate to="/admin" />;
+  }
+}
+
+export default IsPrivate;
