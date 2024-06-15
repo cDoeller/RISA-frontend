fdd7a3e9efcd1e58862673dd40696bc8732be196
Christian Doeller <mail@christiandoeller.de>
2024-06-15 11:00:35 +0200

    added contrib service

---
+++ src/services/contributors.service.js
@@ -0,0 +1,36 @@
+import axios from "axios";
+
+class ContributorsService {
+  // ** BASE URL and Token (to do)
+  constructor() {
+    this.api = axios.create({
+      baseURL: import.meta.env.VITE_API_URL || "http://localhost:5005",
+    });
+
+    // Automatically set JWT token in the headers for every request
+    this.api.interceptors.request.use((config) => {
+      // Retrieve the JWT token from the local storage
+      const storedToken = localStorage.getItem("authToken");
+      if (storedToken) {
+        config.headers = { Authorization: `Bearer ${storedToken}` };
+      }
+      return config;
+    });
+  }
+
+  // ** HTTP Methods
+
+  // GET
+  getAllContributors = () => {
+    return this.api.get("/api/contributors");
+  };
+
+  // CREATE
+  createContributor = (requestBody) => {
+    return this.api.post("/api/contributors", requestBody);
+  };
+}
+
+const contributorsService = new ContributorsService();
+
+export default contributorsService;
