3025bfae93fa241ca8db2ed47ecbcf76061041d3
Christian Doeller <mail@christiandoeller.de>
2024-07-12 12:04:03 +0200

    create news multi image

--- src/pages/CreateNewsPage.jsx
+++ src/pages/CreateNewsPage.jsx
@@ -2,6 +2,7 @@ import react, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import newsService from "../services/news.service";
 import projectsService from "../services/projects.service";
+import cloudinaryService from "../services/cloudinary.services";
 import Select from "react-select";
 import selectStles from "../styles/react-select-styling";
 
@@ -12,6 +13,11 @@ function CreateNewsPage() {
   const [date, setDate] = useState(new Date().toJSON().slice(0, 10));
   const [projectId, setProjectId] = useState([]);
   const [link, setLink] = useState("");
+  // image data
+  const [imageData, setImageData] = useState([]);
+  const [imagePreviews, setImagePreviews] = useState([]);
+  // err
+  const [errorMessage, setErrorMessage] = useState("");
 
   const todayDate = new Date().toJSON().slice(0, 10);
 
@@ -29,25 +35,40 @@ function CreateNewsPage() {
       });
   }, []);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const handleSubmit = async (e) => {
+    try {
+      e.preventDefault();
 
-    const newNews = {
-      title,
-      description,
-      date,
-      //   image_url,
-      related_projects: projectId,
-      link,
-    };
+      const newNews = {
+        title,
+        description,
+        date,
+        related_projects: projectId,
+        link,
+      };
 
-    newsService
-      .createNews(newNews)
-      .then((response) => {
-        console.log(response);
-        navigate("/admin");
-      })
-      .catch((err) => console.log(err));
+      // ******************************************************* ONLY ONE IMAGE
+      if (imageData.length > 0) {
+        // make body formdata for cloudinary route
+        const imageUploadData = new FormData();
+        imageData.forEach((imageFile) => {
+          imageUploadData.append("files", imageFile);
+        });
+        const cloudinaryResponse = await cloudinaryService.uploadMultiple(
+          imageUploadData
+        );
+        newNews.image_url = cloudinaryResponse.data.fileUrls;
+      } else {
+        newNews.image_url = [];
+      }
+
+      const createdNews = await newsService.createNews(newNews);
+      navigate("/admin");
+
+    } catch (err) {
+      setErrorMessage(err.response.data.message);
+      console.log(err);
+    }
   };
 
   // REACT SELECT OPTIONS
@@ -65,6 +86,36 @@ function CreateNewsPage() {
     });
     setProjectId(projectIdArray);
   }
+
+  // * IMAGES FILE UPLOAD
+  function handleImageDelete(index) {
+    const newImageData = [...imageData];
+    const newPreviews = [...imagePreviews];
+    newPreviews.splice(index, 1);
+    newImageData.splice(index, 1);
+    setImageData(newImageData);
+    setImagePreviews(newPreviews);
+  }
+
+  const handleImageInput = (event) => {
+    // make an array of image files from input data
+    event.preventDefault();
+    const files = event.target.files;
+    handleImageData(files);
+  };
+
+  function handleImageData(filesToUpload) {
+    // 1) set the image data for uploading
+    // 2) make the previews
+    const filesArray = Array.from(filesToUpload);
+    const newImageData = [...imageData, ...filesArray];
+    setImageData(newImageData);
+    const previews = newImageData.map((files) => URL.createObjectURL(files));
+    setImagePreviews(previews);
+  }
+
+  // ERRORS
+  const errorMessageElement = <p className="error-message">{errorMessage}</p>;
 
   return (
     <>
@@ -111,8 +162,50 @@ function CreateNewsPage() {
               min={todayDate}
             />
           </label>
-          {/* IMAGE */}
 
+          {/* IMAGES */}
+          <label className="form-input-label">
+            images
+            <input
+              type="file"
+              accept=".jpg, .png"
+              id="create-project-file-input"
+              className="form-file-input-hide"
+              multiple
+              onChange={(event) => {
+                handleImageInput(event);
+              }}
+            />
+            <label
+              className="create-project-file-input-brwose-button pointer"
+              htmlFor="create-project-file-input"
+            >
+              Choose File
+            </label>
+          </label>
+          {/* image previews */}
+          {imagePreviews.length > 0 && (
+            <div className="create-project-image-previews-wrapper flex-column">
+              {imagePreviews.map((previewUrl, index) => {
+                return (
+                  <div
+                    key={previewUrl}
+                    className="create-project-image-previews-img-wrapper flex-row-center-start"
+                  >
+                    <img src={previewUrl} alt={`image preview ${index}`} />
+                    <button
+                      className="create-project-image-previews-delete-button"
+                      onClick={() => {
+                        handleImageDelete(index);
+                      }}
+                    >
+                      X
+                    </button>
+                  </div>
+                );
+              })}
+            </div>
+          )}
           {/* PROJECTS */}
           {/* REACT SELECT */}
           {projectsOptions.length > 0 && (
@@ -138,6 +231,8 @@ function CreateNewsPage() {
               }}
             />
           </label>
+          {/* ERROR MESSAGE */}
+          {errorMessage && errorMessageElement}
           {/* BUTTON */}
           <button className="form-button pointer" type="submit">
             submit
